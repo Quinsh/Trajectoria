a22addfe120ee9c90babdff6edf6430516343daf
HandsomeSB <zhuyiyun060209@gmail.com>
2025-02-08 13:42:33 -0500

    niches

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,6 +7,8 @@ const openai = new OpenAI({
   dangerouslyAllowBrowser: true, // TODO: Move this to backend but for now it's fine
 });
 
+let resume_cache = null;
+
 async function extractWordsFromPdf(file) {
   if (!file) return;
 
@@ -25,17 +27,37 @@ async function extractWordsFromPdf(file) {
 }
 
 /**
+ * 
+ * @param {*} arr array of career paths
+ * @param {*} column column name to perform softmax on
+ */
+function softmaxHelper(arr, column) { 
+    // Run a softmax on the output percentage
+    const exp = [];
+    const multiplier = Math.random() * 4 + 7;
+    for (let i = 0; i < arr.length; i++) {
+      exp.push(Math.exp(arr[i][column] * multiplier));
+      //TODO: Should normalize the data instead of magic number
+    }
+    const expSum = exp.reduce((a, b) => a + b, 0);
+    for (let i = 0; i < arr.length; i++) {
+      arr[i][column] = exp[i] / expSum;
+    }
+    //end softmax
+}
+
+/**
  * Returns a list of career paths based on the resume.
  * @param {*} file
  * @returns
  */
 async function getCareerPaths(file) {
-  const resume = await extractWordsFromPdf(file);
+  resume_cache = await extractWordsFromPdf(file);
 
   const prompt = `
-    Take this resume and tell me in just one word what career path is suitable for me. 
+    Take this resume and tell me in at most two words what career path is suitable for me. 
     Be as specific as possible. For example, AI researcher instead of researcher. 
-    Give top 5 options, along with a percentage of how confident you are in each, and nothing else.
+    Give top 5 options, along with a confidence of how much the resume matches the title, and nothing else. 1 means are you highly confident, 0.5 means you are unsure.
     The data should be formated as a list of json objects, like this:
     {
     "careers" : [{"title": "AI Engineer", "confidence": 0.673} ... ]
@@ -43,10 +65,9 @@ async function getCareerPaths(file) {
     Provide only a JSON response without any markdown formatting or triple backticks.
     `;
 
-  console.log(resume);
   const response = await openai.chat.completions.create({
     model: "gpt-4o",
-    messages: [{ role: "user", content: resume.concat(prompt) }],
+    messages: [{ role: "user", content: resume_cache.concat(prompt) }],
   });
 
   const responseString = response.choices[0].message.content;
@@ -55,36 +76,16 @@ async function getCareerPaths(file) {
   //   "careers" : [{"career": "AI Researcher", "confidence": 0.9}, {"career": "Data Scientist", "confidence": 0.8}, {"career": "Software Engineer", "confidence": 0.7}, {"career": "Machine Learning Engineer", "confidence": 0.6},{"career": "Data Analyst", "confidence": 0.5}]
   //   } // for test
 
-  // Run a softmax on the output percentage
-  const exp = [];
-  const multiplier = Math.random() * 4 + 7;
-  for (let i = 0; i < responseJSON.careers.length; i++) {
-    exp.push(Math.exp(responseJSON.careers[i].confidence * multiplier));
-    //TODO: Should normalize the data instead of magic number
-  }
-  const expSum = exp.reduce((a, b) => a + b, 0);
-  for (let i = 0; i < responseJSON.careers.length; i++) {
-    responseJSON.careers[i].confidence = exp[i] / expSum;
-  }
-  //end softmax
+  softmaxHelper(responseJSON.careers, "confidence");
 
-  console.log(responseJSON);
   return responseJSON;
 }
 
-// export async function analyzeResume(file) {
-//   const careerPaths = await getCareerPaths(file);
-
-//   return careerPaths;
-// }
-
-export async function analyzeResume(file) {
-  const careerPaths = await getCareerPaths(file);
-
+async function peopleSearch(careers) { 
   const results = [];
 
   // Loop through each career path and fetch data
-  for (let careerPath of careerPaths.careers) {
+  for (let careerPath of careers) {
     const url = `/api/v1/mixed_people/search?person_titles[]=${encodeURIComponent(
       careerPath.title
     )}&person_locations[]=United%20States&per_page=3`;
@@ -103,8 +104,6 @@ export async function analyzeResume(file) {
     try {
       const response = await fetch(url, options);
       const data = await response.json();
-
-      console.log(data.people);
 
       // Store all 3 people for each career path
       if (data.people && data.people.length > 0) {
@@ -140,7 +139,41 @@ export async function analyzeResume(file) {
     }
   }
 
-  careerPaths["contacts"] = results;
+  return results;
+}
+
+async function getNiche(topCareer) {
+  const prompt = `
+  Take this resume and this career path: ${topCareer}, tell me in at most two words what niche within that career is suitable for me. 
+  Be as specific as possible. For example, computing architecture instead of backend engineer. 
+  Give top 5 options, along with a confidence of how much the resume matches the niche, and nothing else. 1 means are you highly confident, 0.5 means you are unsure.
+  The data should be formated as a list of json objects, like this:
+  {
+  "niches" : [{"title": "Title1", "confidence": 0.673} ... ]
+  }
+  Provide only a JSON response without any markdown formatting or triple backticks.
+  `;
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [{ role: "user", content: resume_cache.concat(prompt) }],
+  });
+
+  const responseJSON = JSON.parse(response.choices[0].message.content);
+
+  softmaxHelper(responseJSON.niches, "confidence");
+
+  return responseJSON;
+}
+
+export async function analyzeResume(file) {
+  const careerPaths = await getCareerPaths(file);
+  // const results = await peopleSearch(careerPaths.careers);
+  const [contacts, niches] = await Promise.all([peopleSearch(careerPaths.careers), getNiche(careerPaths.careers[0].title)]);
+  
+
+  careerPaths["contacts"] = contacts;
+  careerPaths["niches"] = niches;
   console.log(careerPaths);
   return careerPaths;
 }
